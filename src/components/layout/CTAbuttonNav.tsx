ce98dab7dead8e27bd4ee6edfeb18e41748dbc61
developeritexpert <developeritexpert3@gmail.com>
2026-02-09 13:45:59 +0530

    some more points fixes

--- src/components/layout/CTAbuttonNav.tsx
+++ src/components/layout/CTAbuttonNav.tsx
@@ -9,13 +9,13 @@ export default function CTAbuttonNav({ href, text }: CTAbuttonProps) {
   return (
     <Link
       href={href}
-      className="group relative block flex h-[45px] w-full max-w-[120px] items-center justify-center overflow-hidden rounded-[7px] bg-white text-black transition-colors duration-300 hover:text-white hover:underline"
+      className="group relative block flex h-[40.5px] w-full max-w-[120.7px] items-center justify-center overflow-hidden rounded-[7px] bg-white px-[2rem] py-[0.75rem] text-black transition-colors duration-300 hover:text-white hover:underline"
     >
       {/* black sliding background */}
       <span className="absolute inset-0 translate-y-[101%] bg-[#00B0B2] transition-transform duration-300 ease-in-out group-hover:translate-y-0" />
 
       {/* text */}
-      <span className="relative z-10 text-[15px]">{text}</span>
+      <span className="relative z-10 text-[14px]">{text}</span>
     </Link>
   );
 }
